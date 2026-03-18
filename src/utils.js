35bb991fdc923365543f300f1037bce2b0622e5b
bkporter12 <86490115+bkporter12@users.noreply.github.com>
2026-03-18 08:42:48 -0700

    Increment currentPractice from 50 to 51
    Updated currentPractice variable from 50 to 51.

--- src/utils.js
+++ src/utils.js
@@ -59,7 +59,7 @@ export function balanceAndSortJudges(judges) {
   });
 
   let currentOfficial = 1;
-  let currentPractice = 50;
+  let currentPractice = 51; // Updated from 50 to 51
 
   balanced.forEach(j => {
     if (j.Type === 'Official') {
